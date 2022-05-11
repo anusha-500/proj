fa0755160fadfcc1766a9d5ff6d5b4976151aadd
Anusha-500 <anusha.kodamala@mantragroup.com>
2022-05-11 11:13:26 +0530

    exam

---
+++ src/components/question6.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="app">
+Enter Value:<input type="text" v-model="message" placeholder="enter value" required>
+</div>
+</template>
+<script>
+export default {
+name: "Question6AA",
+data() {
+    return{
+    message:"",
+    }
+},
+watch: {
+message(newvalue, oldvalue) {
+console.log("newvalue:",newvalue);
+console.log("oldvalue:",oldvalue);
+}
+}
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import question5 from '../components/question5'
-//import watchUpper from '../components/watchUppercase.vue'
+import question6 from '../components/question6'
 Vue.use(Router)
 const routes =[
 {
@@ -9,6 +9,11 @@ path:'/question5',
 name:'question5',
 component:question5
 },
+{
+    path:'/question6',
+    name:'question6',
+    component:question6
+    },
 ]
 let router = new Router({routes})
 export default router
